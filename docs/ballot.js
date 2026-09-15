42ab34c0cf26061316a0154594b9e45aa34edff3
Emily Toto <etoto@isa.org>
2026-09-15 14:56:45 -0400

    fix again

--- docs/ballot.js
+++ docs/ballot.js
@@ -99,7 +99,7 @@ if (invitationToken && ballotId) {
         `register.html?invite=${encodeURIComponent(invitationToken)}` +
         `&voter=${encodeURIComponent(voterId)}` +
         `&ballot=${encodeURIComponent(ballotId)}` +
-        `&interest=${encodeURIComponent(interest || "")}
+        `&interest=${encodeURIComponent(interest || "")}`
 
     return;
 }
