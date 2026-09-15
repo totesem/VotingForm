ba991d97098b625b5fc13b339871dedfbac2866e
Emily Toto <etoto@isa.org>
2026-09-15 17:40:13 -0400

    fix everything8

--- docs/ballot.js
+++ docs/ballot.js
@@ -27,12 +27,14 @@ const switchButton = document.getElementById("switchAccount");
 
 const {
     data: { session },
-    error
-} = await supabaseClient.auth.getSession();
+    error: sessionError
+} = await supabaseClient.auth.refreshSession();
 
-if (error) {
-    console.error(error);
-    message.textContent = "Supabase connection error.";
+if (sessionError || !session) {
+
+    message.textContent =
+        "You must register or sign in to vote.";
+
     return;
 }
 
